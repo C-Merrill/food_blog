79ef005a0ec56b95c77230f513a38c95f3072f1c
cs2610 <cmerrill99@gmail.com>
2016-04-03 17:28:32 -0600

    Add first half of files

---
+++ routes/articles.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var router = express.Router();
+
+
+//var entries = [{slug:"How to pass class", body: "Come to class. Do your homework"},{slug:"February 12, 2016", body:"There are options for free web hosting, such as Heroku. I was under the impression that I would have to pay monthly to put any website on the web, but it seems I can do it free."},{slug:"February 13,2016", body:"I've been using Emacs for a while, but never took the time to master the keyboard shortcuts. Today, I learned how to open files using C-x C-f and how to switch between open buffers, so I can edit multiple files at once. Because there is no visual interface evidence of other open buffers, I assumed that you can only have one file open at a time. Now I can work much more productively in this editor."},{slug:"March 4, 2016", body:"Learned all about rendering sites in javascript to make this site work."}];
+var entries=[];
+
+/* READ all: GET entries listing. */
+router.get('/', function(req, res, next){
+    var name = req.cookies.username | '';
+    req.db.driver.execQuery(
+	     "SELECT * FROM articles;",
+	      function(err, data){
+	         if(err){
+		           console.log(err);
+	         }
+	         res.render('articles/index', { title: 'Articles', articles: data, name: name });
+	      }
+    );
+});
+
+/* CREATE entry form: GET /til/new */
+router.get('/new', function(req, res, next) {
+    res.render('articles/new', { title: "Enter new article"});
+});
+
+/*CREATE entry: POST /til/ */
+router.post('/', function(req, res, next) {
+    req.db.driver.execQuery(
+      "INSERT INTO articles (headline,body) VALUES (?,?);",
+      [req.body.slug,req.body.body],
+      function(err,data){
+        if(err)
+        {
+          console.log(err);
+        }
+      }
+    );
+
+    res.redirect(303,'/articles');
+});
+
+/* UPDATE entry form: GET /til/1/edit */
+router.get('/:id/edit', function(req, res, next) {
+  req.db.driver.execQuery(
+    "SELECT * FROM articles WHERE id=?",
+    [parseInt(req.params.id)],
+    function(err, data){
+      if(err){
+        console.log(err);
+      }
+      res.render('articles/update', { title: 'Update an entry', article: data[0] });
+    }
+  )
+});
+
+/* UPDATE entry: POST /til/1 */
+router.post('/:id', function(req, res, next){
+  req.db.driver.execQuery(
+    "UPDATE articles SET (headline,body)= (?,?) WHERE id=?;",
+    [req.body.slug,req.body.body,parseInt(req.params.id)],
+    function(err, data){
+      if (err){
+        console.log(err);
+      }
+    }
+  );
+
+  res.redirect(303,'/articles/' + req.params.id);
+});
+
+/*DELETE entry: GET /til/1/delete */
+router.get('/:id/delete', function(req, res, next){
+  req.db.driver.execQuery(
+    'DELETE FROM articles WHERE id=?;',
+    [parseInt(req.params.id)],
+    function(err, data){
+      if (err){
+        console.log(err);
+      }
+    }
+  );
+  res.redirect(303,'/articles');
+});
+
+/* READ one entry: GET /til/0 */
+router.get('/:id', function(req, res, next) {
+    req.db.driver.execQuery(
+      "SELECT * FROM articles WHERE id=?;",
+      [parseInt(req.params.id)],
+      function(err, data){
+        if (err){
+          console.log(err);
+        }
+        res.render('artices/article', { title: 'Logan Foodies', article: data[0]});
+      }
+    );
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var crypto = require('crypto');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var name = req.cookies.username;
+    res.render('index', { title: 'Logan Foodies', name: name});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login - Logan Foodies'});
+});
+
+router.post('/login', function(req, res, next) {
+  var sha1sum = crypto.createHash('sha1');
+  var salt = 'lP7E17apjwkm2NIk1NFv';
+
+  req.db.driver.execQuery(
+    'SELECT * FROM users WHERE username=?;',
+    [req.body.username],
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+
+      sha1sum.update(req.body.password+salt);
+      var hashed_input = sha1sum.digest('hex');
+
+      if(data[0]!=null && hashed_input === data[0].password) //DONT Do this in other projects!!!
+      {
+        res.cookie('username', data[0].name);
+        res.redirect('/articles/');
+      }
+      else
+      {
+        res.redirect('/login');
+      }
+    }
+  );
+
+});
+
+router.get('/logout', function(req,res){
+  res.clearCookie('username');
+  res.redirect('/');
+})
+
+module.exports = router;
